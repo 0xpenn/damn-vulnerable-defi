a1afd0bb9fce4482da93d9868fa13edd4002d6c0
0xpenn <arojooreoluwa@email.com>
2026-08-01 19:37:18 +0100

    Solve DVDF Challenge 3 (Truster) - arbitrary call self-approve exploit

--- test/truster/Truster.t.sol
+++ test/truster/Truster.t.sol
@@ -5,6 +5,7 @@ pragma solidity =0.8.25;
 import {Test, console} from "forge-std/Test.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 import {TrusterLenderPool} from "../../src/truster/TrusterLenderPool.sol";
+import {TrusterAttack} from "../../src/truster/TrusterAttack.sol";
 
 contract TrusterChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -51,7 +52,7 @@ contract TrusterChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_truster() public checkSolvedByPlayer {
-        
+ new TrusterAttack(address(pool), address(token), recovery);        
     }
 
     /**
